accbb52ad7ef16444316660a97e1709a74db39e2
ismalee <ismalee@github.com>
2025-07-13 13:23:42 +0300

    Add hooks.py to correct package directory for Frappe app

---
+++ seidit_zatca_module/hooks.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+app_name = "seidit_zatca_module"
+app_title = "SEIDiT ZATCA Module"
+app_publisher = "SEIDiT"
+app_description = "SEIDiT ZATCA Phase 2 Module for ERPNext"
+app_icon = "octicon octicon-file-directory"
+app_color = "blue"
+app_email = "support@seidit.com"
+app_license = "Proprietary" 
