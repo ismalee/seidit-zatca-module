6c6db0b3beda907e0ac176ca438c4949c462efbc
ismalee <ismalee@github.com>
2025-07-13 12:53:44 +0300

    Add setup.py for Frappe app installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seidit_zatca_module',
+    version='2.0.0',
+    description='SEIDiT ZATCA Phase 2 Module for ERPNext',
+    author='SEIDiT',
+    author_email='support@seidit.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+) 
